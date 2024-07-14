6fdd442c463e8a4cafbb75adb0303b6a284ff029
Habatakurikei <131997581+Habatakurikei@users.noreply.github.com>
2024-07-14 16:18:30 +0200

    Create config.py

---
+++ zoltraakklein/config.py
@@ -0,0 +1,18 @@
+# デフォルト言語モデルとパラメータ
+DEFAULT_PROVIDER = "anthropic"
+DEFAULT_MODEL = "claude-3-5-sonnet-20240620"
+DEFAULT_TEMPERATURE = 0.7
+
+# 各種パス設定
+PATH_TO_COMPILER = "compiler"
+PATH_TO_PROMPT = "prompts"
+PATH_TO_REQUIREMENT = "requirements"
+PATH_TO_GENERATED = "generated"
+PATH_TO_MAGIC = "generated/requirements"
+
+# 各種プロンプトファイルの指定
+FORMATTER = "md_comment.md"
+PROMPT_FOR_NAME = "name_for_requirement_prompt.md"
+
+# 領域展開して良いコンパイラリスト
+DOMAIN_COMPILER_LIST = []
